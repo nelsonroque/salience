58acff49cabc9e24746f23c18f55c257587cb8f3
Nelson Roque <nelsonroquejr@gmail.com>
2018-10-03 23:55:00 -0400

    thoughts for functions

--- channelExtraction.R
+++ channelExtraction.R
@@ -1,13 +1,20 @@
 library(imager)
-#Sample functions and turn them into separate R,G,B channels
-R <- as.cimg(function(x,y) cos(sin(cos(6*x*y^3))),100,100)
-G <- as.cimg(function(x,y) cos(cos(3*x*y*x*y^9*y + pi^4/2^0.1)),100,100)
-B <- as.cimg(function(x,y) cos(exp(-.03*x^4)),100,100)
-trippy <- imappend(list(R,G,B),"c") #Bind the three channels into one image
-plot(trippy)
 
-R <- as.cimg(function(x,y) seq(1,10000,1),100,100)
-G <- as.cimg(function(x,y) seq(1,10000,1),100,100)
-B <- as.cimg(function(x,y) seq(1,10000,1),100,100)
-gradient <- imappend(list(R,G,B),"c") #Bind the three channels into one image
-plot(gradient)
+#' @param img class: img loaded with imager plugin
+#' @param channels class: vector, vector of characters ('R','G','B','RGB')
+extractChannels <- function(img) {
+}
+
+#' @param img class: img loaded with imager plugin
+#' @param threshold class: numeric; 
+percentage.above.threshold <- function(img,threshold,direction) {
+}
+
+#' @param img class: img loaded with imager plugin
+#' @param threshold class: numeric; 
+#' @param func class: string
+feature_extraction <- function(img,channel,func) {
+}
+
+# combine features (pooling)
+
